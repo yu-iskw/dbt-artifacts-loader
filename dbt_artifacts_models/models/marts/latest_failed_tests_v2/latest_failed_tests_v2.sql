1a4c6d403536b27efd09bb3af8dadc825830bd68
Yu ISHIKAWA <yuu.ishikawa@gmail.com>
2021-08-18 10:28:22 +0900

    Update

--- dbt_artifacts_models/models/marts/latest_failed_tests_v2/latest_failed_tests_v2.sql
+++ dbt_artifacts_models/models/marts/latest_failed_tests_v2/latest_failed_tests_v2.sql
@@ -8,7 +8,7 @@
     materialized="view",
     database=project,
     schema=dataset,
-    alias="latest_run_results_v2",
+    alias="latest_failed_tests_v2",
     persist_docs={"relation": true, "columns": true},
     labels={
       "modeled_by": "dbt",
@@ -17,20 +17,18 @@
   )
 }}
 
-WITH latest_run_results_v1 AS (
+WITH latest_failed_tests AS (
   SELECT
-    ROW_NUMBER() OVER (PARTITION BY unique_id ORDER BY completed_at DESC) AS rank
-    , invocation_id
-    , dbt_version
-    , rpc_method
-    , unique_id
-    , status
-    , timing_name
-    , completed_at
-    , started_at
+    ROW_NUMBER() OVER (PARTITION BY unique_id ORDER BY completed_at DESC) AS rank,
+    *
   FROM {{ ref("expanded_run_results_v2") }}
+  WHERE
+    timing_name = "execute"
+    AND rpc_method IN ("test")
+    AND LOWER(status) = "fail"
 )
 
-SELECT *
-FROM latest_run_results_v1
+SELECT
+  * EXCEPT (rank)
+FROM latest_failed_tests
 WHERE rank = 1
