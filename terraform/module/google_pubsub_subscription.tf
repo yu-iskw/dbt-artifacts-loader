34b1e0c9b15daac68a7136d80ac2d192a6456325
Yu ISHIKAWA <yuu.ishikawa@gmail.com>
2021-10-12 15:40:10 +0900

    Update

--- terraform/module/google_pubsub_subscription.tf
+++ terraform/module/google_pubsub_subscription.tf
@@ -5,12 +5,26 @@ resource "google_pubsub_subscription" "dbt_artifacts_notification_to_bigquery" {
   topic = google_pubsub_topic.dbt_artifacts_notification.id
 
   push_config {
-    push_endpoint = "${google_cloud_run_service.dbt_artifact_loader.status[0].url}/api/v1/"
+    push_endpoint = "${google_cloud_run_service.dbt_artifact_loader.status[0].url}/api/v2/"
 
     oidc_token {
       service_account_email = google_service_account.cloud_run_invoker.email
     }
   }
+
+  dead_letter_policy {
+    dead_letter_topic = google_pubsub_topic.dbt_artifacts_notification_deadletter.id
+    max_delivery_attempts = 5
+  }
+
+  labels = var.labels
+}
+
+resource "google_pubsub_subscription" "dbt_artifacts_notification_deadletter_pool" {
+  project = var.project_id
+
+  name  = "${var.pubsub_topic}-deadletter-pool"
+  topic = google_pubsub_topic.dbt_artifacts_notification_deadletter.id
 
   labels = var.labels
 }
--- terraform/module/google_pubsub_topic.tf
+++ terraform/module/google_pubsub_topic.tf
@@ -4,3 +4,10 @@ resource "google_pubsub_topic" "dbt_artifacts_notification" {
   name   = var.pubsub_topic
   labels = var.labels
 }
+
+resource "google_pubsub_topic" "dbt_artifacts_notification_deadletter" {
+  project = var.project_id
+
+  name   = "${var.pubsub_topic}-deadletter"
+  labels = var.labels
+}
