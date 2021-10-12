aa0756a1c300561c2dbabf428c4e170a5d4ef020
Yu ISHIKAWA <yuu.ishikawa@gmail.com>
2021-10-12 21:07:50 +0900

    tmp

--- dbt_artifacts_models/models/marts/v2/schema_test_run_results_v2/schema_test_run_results_v2.sql
+++ dbt_artifacts_models/models/marts/v2/schema_test_run_results_v2/schema_test_run_results_v2.sql
@@ -17,16 +17,36 @@
   )
 }}
 
-WITH run_results AS (
+WITH schema_tests AS (
+  SELECT
+    * EXCEPT(depends_on, depends_on_macro, depends_on_node),
+    depends_on_macro,
+    depends_on_node,
+  FROM {{ ref("parsed_schema_test_node_v2") }} AS schema_test_nodes
+       CROSS JOIN UNNEST(depends_on.nodes.value) AS depends_on_node
+       CROSS JOIN UNNEST(depends_on.macros.value) AS  depends_on_macro
+)
+, schema_tests_with_models AS (
+  SELECT
+    schema_tests.* EXCEPT(metadata),
+    schema_tests.metadata AS schema_test_metadata,
+    models.* EXCEPT(metadata, unique_id),
+    models.metadata AS model_metadata,
+    models.unique_id AS model_unique_id,
+  FROM schema_tests AS schema_tests
+  LEFT OUTER JOIN {{ ref("parsed_model_node_v2") }} AS models
+    ON schema_tests.depends_on_node = models.unique_id
+       AND schema_tests.metadata.invocation_id = models.metadata.invocation_id
+)
+, run_results AS (
   SELECT
     run_results.* EXCEPT(metadata),
     run_results.metadata AS run_results_metadata,
-    manifest.* EXCEPT(metadata, unique_id, test_metadata),
-    manifest.metadata AS manifest_metadata,
+    schema_tests_with_models.* EXCEPT(unique_id),
   FROM {{ ref("expanded_run_results_v2") }} AS run_results
-  LEFT OUTER JOIN {{ ref("parsed_schema_test_node_v2") }} AS manifest
-    ON run_results.unique_id = manifest.unique_id
-  WHERE manifest.unique_id IS NOT NULL
+  LEFT OUTER JOIN schema_tests_with_models AS schema_tests_with_models
+    ON run_results.unique_id = schema_tests_with_models.unique_id
+  WHERE schema_tests_with_models.unique_id IS NOT NULL
 )
 , nearest_manifests AS (
   SELECT
@@ -34,7 +54,7 @@ WITH run_results AS (
     * EXCEPT(generated_at_diff)
   FROM (
       SELECT
-        ABS(DATETIME_DIFF(run_results_metadata.generated_at, manifest_metadata.generated_at, SECOND)) AS generated_at_diff,
+        ABS(DATETIME_DIFF(run_results_metadata.generated_at, schema_test_metadata.generated_at, SECOND)) AS generated_at_diff,
         *,
       FROM run_results
   )
