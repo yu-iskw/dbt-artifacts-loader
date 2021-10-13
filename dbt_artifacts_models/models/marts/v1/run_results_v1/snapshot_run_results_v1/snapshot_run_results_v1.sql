c5ad7630ca59376ee51d0df7ec9b6365b0f1f2d4
Yu Ishikawa <yu-iskw@users.noreply.github.com>
2021-10-13 14:52:38 +0900

    Fix dbt models (#18)

--- dbt_artifacts_models/models/marts/v1/run_results_v1/snapshot_run_results_v1/snapshot_run_results_v1.sql
+++ dbt_artifacts_models/models/marts/v1/run_results_v1/snapshot_run_results_v1/snapshot_run_results_v1.sql
@@ -19,24 +19,23 @@
 
 WITH run_results AS (
   SELECT
-    run_results.* EXCEPT(metadata),
-    run_results.metadata AS run_results_metadata,
-    snapshots.* EXCEPT(metadata, unique_id, loaded_at),
-    snapshots.metadata AS snapshot_metadata,
+    run_results.*,
+    (SELECT AS STRUCT snapshots.*) AS snapshot,
   FROM {{ ref("expanded_run_results_v1") }} AS run_results
-  LEFT OUTER JOIN {{ ref("parsed_snapshot_node_v1") }} AS snapshots
+  FULL OUTER JOIN {{ ref("parsed_snapshot_node_v1") }} AS snapshots
     ON run_results.unique_id = snapshots.unique_id
+      AND ABS(DATETIME_DIFF(run_results.metadata.generated_at, snapshots.metadata.generated_at, DAY))  <= 2
   WHERE snapshots.unique_id IS NOT NULL
     AND timing_name IN ("execute")
 )
 -- Extract only run results whose metadata is the most close to that of model.
 , nearest_manifests AS (
   SELECT
-    ROW_NUMBER() OVER (PARTITION BY unique_id ORDER BY generated_at_diff) AS rank,
+    ROW_NUMBER() OVER (PARTITION BY unique_id, metadata.invocation_id ORDER BY generated_at_diff) AS rank,
     * EXCEPT(generated_at_diff)
   FROM (
       SELECT
-        ABS(DATETIME_DIFF(run_results_metadata.generated_at, snapshot_metadata.generated_at, SECOND)) AS generated_at_diff,
+        ABS(DATETIME_DIFF(metadata.generated_at, snapshot.metadata.generated_at, SECOND)) AS generated_at_diff,
         *,
       FROM run_results
   )
