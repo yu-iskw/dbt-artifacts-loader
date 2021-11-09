7947858f50a16c706b5caa364083eb2c4b819def
Yu Ishikawa <yu-iskw@users.noreply.github.com>
2021-11-09 16:57:53 +0900

    Implement source_freshness_output_v2 (#25)

---
+++ dbt_artifacts_models/models/staging/v2/sources_v2/source_freshness_output_v2/source_freshness_output_v2.sql
@@ -0,0 +1,39 @@
+{% set project = var('dbt_artifacts_loader')['project'] %}
+{% set dataset = var('dbt_artifacts_loader')['dataset'] %}
+
+{{
+  config(
+    enabled=true,
+    full_refresh=none,
+    materialized="view",
+    database=project,
+    schema=dataset,
+    alias="source_freshness_output_v2",
+    persist_docs={"relation": true, "columns": true},
+    labels={
+      "modeled_by": "dbt",
+      "app": "dbt-artifacts-loader",
+     },
+  )
+}}
+
+WITH expanded_sources AS (
+  SELECT
+    loaded_at AS loaded_at,
+    elapsed_time,
+    metadata.*,
+    result.values.SourceFreshnessOutput.*,
+  FROM {{ source(var('dbt_artifacts_loader')['dataset'], 'sources_v2') }}
+        , UNNEST(results) AS result
+)
+, remove_duplicates AS (
+  SELECT
+    ROW_NUMBER() OVER (PARTITION BY invocation_id, unique_id ORDER BY generated_at DESC) AS rank,
+    *,
+  FROM expanded_sources
+)
+
+SELECT
+  * EXCEPT(rank)
+FROM remove_duplicates
+WHERE rank = 1
