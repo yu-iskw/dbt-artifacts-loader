a20256b0f421baccc62b86abf796b97f94660abd
Yu ISHIKAWA <yuu.ishikawa+github@gmail.com>
2023-04-04 08:40:15 +0900

    Add variables to set the dataset owners and readers
    Signed-off-by: Yu ISHIKAWA <yuu.ishikawa+github@gmail.com>

--- terraform/module/google_bigquery_dataset_access.tf
+++ terraform/module/google_bigquery_dataset_access.tf
@@ -5,3 +5,21 @@ resource "google_bigquery_dataset_access" "cloud_run_is_dataset_owner" {
   role          = "OWNER"
   user_by_email = google_service_account.dbt_artifacts_loader_cloud_run.email
 }
+
+resource "google_bigquery_dataset_access" "dataset_owners" {
+  for_each = toset(var.bigquery_dataset_owners)
+
+  project       = var.project_id
+  dataset_id    = google_bigquery_dataset.dbt_artifacts.dataset_id
+  role          = "OWNER"
+  user_by_email = each.value
+}
+
+resource "google_bigquery_dataset_access" "dataset_readers" {
+  for_each = toset(var.bigquery_dataset_readers)
+
+  project       = var.project_id
+  dataset_id    = google_bigquery_dataset.dbt_artifacts.dataset_id
+  role          = "READER"
+  user_by_email = each.value
+}
